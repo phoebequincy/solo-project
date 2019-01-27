b849340c85140f4326de0103c033f97944c1774a
Phoebe Quincy <phoebequincy@gmail.com>
2019-01-26 23:23:27 -0700

    Cleaned up boilerplate, added intial files.

---
+++ src/components/background.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Background extends Component {
+
+
+
+    render() {
+        return (
+          <div class="container-fluid">
+            <div class="parallax">
+              <div id="stars1"></div>
+              <div id="stars2"></div>
+              <div id="stars3"></div>
+            </div>
+
+          </div>
+        )
+    }
+}
+
+export default Background;
